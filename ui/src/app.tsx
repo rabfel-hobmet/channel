109d1c582fa1ecd137767601961c0c7fea476658
Matilde Park <m@park.computer>
2022-06-05 14:28:03 -0700

    thread: add thread scaffold

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import { Board } from './board'
 import { Home } from "./home";
+import { Thread } from "./thread";
 import Urbit from '@urbit/http-api';
 
 const api = new Urbit('', '', window.desk);
@@ -18,6 +19,7 @@ export function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/board/:ship/:board" element={<Board/>}/>
+      <Route path="/thread/:ship/:board/:index" element={<Thread/>}/>
     </Routes>
   </BrowserRouter>
   )
---
+++ ui/src/thread.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { graph } from '@urbit/api';
+
+export function Thread() {
+  const [thread, setThread] = useState([]);
+  const [op, setOp] = useState();
+  const [reply, setReply] = useState("");
+  const {ship, board, index} = useParams();
+
+  useEffect(() => {
+    async function init() {
+      const msg = await window.api.scry(graph.getNode(ship, board, `/${index}`));
+      Object.entries(msg["graph-update"]["add-nodes"]["nodes"]).map(([key, value]) => {
+        const op = value["children"][1]["children"][1]["post"]["contents"][1].text;
+        setOp(op);
+      });
+    };
+    init()
+  }, [ship, board, index])
+
+  const submitReply = () => {
+      window.api.poke({
+          app: "channel-client",
+          mark: "channel-stacy",
+          json: {
+              "add-poast": {
+                  "maybe-index": index,
+                  message: [{text: reply}],
+                  ship: ship,
+                  board: board
+              }
+          }
+      })
+  }
+
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <p>{op}</p>
+      <textarea className="border p-2 border-black rounded-xl max-w-prose" onChange={e => setReply(e.target.value)}/>
+      <button className="bg-gray-100 rounded-xl max-w-prose p-2" onClick={submitReply}>submit</button>
+    </main>
+  );
+}
