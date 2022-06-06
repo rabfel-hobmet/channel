09ee8a06df538774df0c5d500f4d1e137fd72a0e
Matilde Park <m@park.computer>
2022-06-06 00:23:13 -0700

    home: stubbed add provider

--- ui/src/home.tsx
+++ ui/src/home.tsx
@@ -30,16 +30,19 @@ export function Home() {
 
     return (
         <main className="flex flex-col items-center justify-center min-h-screen px-4 space-y-4 lg:space-y-0">
-            <Marquee gradient={false} className="static lg:absolute min-w-screen top-4 -right-8 lg:rotate-12 border-b-4 bg-black border-black" speed={200}>
+            <Marquee gradient={false} className="static lg:absolute min-w-screen pt-96 -top-80 -right-44 lg:rotate-12 border-b-4 bg-black border-black" speed={200}>
             <h2 className="text-8xl text-ridge-yellow font-bold">CHANNEL</h2>
             </Marquee>
             <h2 className="bg-black w-screen static lg:absolute text-ridge-yellow text-4xl z-0 border-y-4 border-double border-ridge-yellow">CHOOSE YOUR PROVIDER</h2>
-        <div className="w-full max-w-prose border rounded-xl p-2 py-16 my-16 bg-ridge-yellow border-black z-10">
+        <div className="w-full flex flex-col overflow-y-auto justify-between h-96 max-w-prose border rounded-xl p-2 my-16 bg-ridge-yellow border-black z-10">
+            <div>
             {Object.entries(boards).map(([key, value]) => {
                 return <><h2 className="font-semibold font-mono" key={key}>{key}</h2>
                 {value["boards"].map((board) => <Link className="text-red-500" to={`/board/${key}/${board.board}`}>/{board.board}/</Link>)}
                 </>
             })}
+            </div>
+            <p className='uppercase font-bold text-right'>Add provider</p>
         </div>
         <p>Use at your own risk.</p>
         </main>
