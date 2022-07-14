5b1d5a329873e14c84871976b34647e5f4c60763
Matilde Park <m@park.computer>
2022-07-14 11:47:01 -0700

    boards: add names

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Board } from "./board";
 import { Home } from "./home";
@@ -12,12 +12,18 @@ import { useFileStore } from "./state/useFileStore";
 import "./index.css";
 
 export function App() {
+  const [providers, setProviders] = useState<string[]>([]);
+  const [boards, setBoards] = useState([]);
   const { s3 } = useStorageState();
   const credentials = s3.credentials;
   const { createClient } = useFileStore();
+
   useEffect(() => {
     async function init() {
       useStorageState.getState().initialize(api);
+      addEventListener('beforeunload', e => {
+        api.reset()
+      })
     }
 
     init();
@@ -35,11 +41,35 @@ export function App() {
     }
   }, [credentials]);
 
+  useEffect(() => {
+    const init = async () => {
+      const providers = await api.scry({
+        app: "channel-client",
+        path: "/providers",
+      });
+      setProviders(providers["providers"]);
+    };
+      init();
+  }, [api]);
+
+  useEffect(() => {
+    const init = async () => {
+      const bards = [];
+      const newBoards = await providers.reduce((prevPromise, nextBoard) => {
+        return prevPromise.then(async () => {
+          return bards.push({ship: nextBoard, boards: await window.api.scry({ app: "channel-client", path: `/boards/${nextBoard}`}).then((res) => res.boards)})
+        })
+      }, Promise.resolve())
+        setBoards(bards);
+    };
+    init();
+  }, [providers, api]);
+
   return (
     <BrowserRouter basename="/apps/channel">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/board/:ship/:board" element={<Board />} />
+        <Route path="/" element={<Home providers={providers} boards={boards}/>} />
+        <Route path="/board/:ship/:board" element={<Board boards={boards} />} />
         <Route path="/thread/:ship/:board/:index" element={<Thread />} />
         <Route path="/list/:ship/:board" element={<List />} />
         <Route path="/catalog/:ship/:board" element={<Catalog />} />
--- ui/src/components/navbar.tsx
+++ ui/src/components/navbar.tsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { NavLink } from "react-router-dom";
 
-export default function ChannelNav({ ship, board }) {
+export default function ChannelNav({ ship, board, boards }) {
   let inactiveClassName =
     "text-2xl font-bold text-link-blue hover:text-link-hover hover:underline";
   let activeClassName = "text-chan-red text-2xl";
 
+  const name = boards.find((e) => e.ship === ship)?.boards.find((e) => e.board === board).name || "";
   return (
     <ul className="flex md:flex-row flex-col my-3 md:pl-9 divide-x-2">
       <li className="px-3">
@@ -16,7 +17,7 @@ export default function ChannelNav({ ship, board }) {
             !isActive ? inactiveClassName : activeClassName
           }
         >
-          /{board}/
+          /{board}/ {name && `- ${name}`}
         </NavLink>
       </li>
       <li className="px-3">
@@ -40,9 +41,9 @@ export default function ChannelNav({ ship, board }) {
         </NavLink>
       </li>
       <li className="px-3">
-        <a href="/apps/channel/" className={inactiveClassName}>
+        <NavLink to="/" className={inactiveClassName}>
           home
-        </a>{" "}
+        </NavLink>{" "}
       </li>
     </ul>
   );
